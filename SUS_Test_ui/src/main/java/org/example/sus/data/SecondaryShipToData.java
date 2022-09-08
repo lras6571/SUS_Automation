bcf34536535eb6175da97b539606cee6614a15ba
lras6571 <lasan.rashmika@syscolabs.com>
2022-09-08 20:56:44 +0530

    Assert ShipToNumber - lasan01

---
+++ SUS_Test_ui/src/main/java/org/example/sus/data/SecondaryShipToData.java
@@ -0,0 +1,62 @@
+package org.example.sus.data;
+
+/**
+ * Created: dilini withana
+ * Date: 9/15/2019
+ * Modified: subbiah.resitha
+ * Date: 11/11/2019
+ **/
+
+public class SecondaryShipToData {
+
+    private SecondaryShipToData(){}
+
+    //secondary ship-to account details
+    public static final String CMU_WRONG_ACCOUNT_NAME = "TEST@#$@#$%@%@#%@!";
+    public static final String SHIP_TO_ACCOUNT_NAME= "JOHN_TEST";
+    public static final String SHIP_TO_STREET_NAME = "1070 N MAIN ST";
+    public static final String SHIP_CITY = "RANDOLPH";
+    public static final String SHIPPING_STATE_OPTION_01 = "MA";
+    public static final String SHIP_POSTAL_CODE = "02368";
+    public static final String STORE_NUMBER = "12367854";
+    public static final String STORE_PHONE = "0382233445";
+
+    //principal contact
+    public static final String FIRST_NAME = "JOHN";
+    public static final String LAST_NAME = "Test";
+    public static final String EMAIL_ADDRESS = "mdm.testuser100@gmail.com";
+    public static final String TEL_NUMBER_01 = "(628) 246 2222";
+    public static final String TEL_NUMBER_02 = "01123456789";
+
+
+    //delivery preferences
+    public static final String DELIVERY_DATE = "Mon";
+    public static final String DELIVERY_WINDOW_START_TIME = "01:30";
+    public static final String DELIVERY_WINDOW_END_TIME = "02:30";
+    public static final String BACK_DOOR_OPEN_TIME = "03:30";
+    public static final String BACK_DOOR_CLOSE_TIME = "05:30";
+
+    //Ship To Account owner
+    public static final String ACCOUNT_OWNER = "Informatica Load";
+
+    //Ship-to confirmation
+    public static final String CONFIRM_ACCOUNT_NAME = "Name";
+    public static final String CONFIRM_ACCOUNT_TYPE = "Account Type";
+    public static final String CONFIRM_ACCOUNT_PHONE = "Phone";
+    public static final String CONFIRM_ACCOUNT_STORE_NUMBER = "Store Number";
+    public static final String CONFIRM_SHIPPING_STREET = "Shipping Street";
+    public static final String CONFIRM_SHIPPING_CITY = "Shipping City";
+    public static final String CONFIRM_SHIPPING_POSTAL_CODE = "Shipping Postal Code";
+    public static final String CONFIRM_SHIPPING_COUNTRY = "Shipping Country";
+    public static final String CONFIRM_TYPE_OF_CUISINE = "Type of Cuisine";
+    public static final String CONFIRM_TYPE_OF_OPERATION = "Type of Operation";
+    public static final String CONFIRM_ACCOUNT_EMAIL = "Email";
+    public static final String CONFIRM_CONCEPT_ID = "Concept ID";
+    public static final String DELIVERY_DAY = "Delivery Day";
+    public static final String STANDARD_DELIVERY_WINDOW_START = "Delivery Window Start Time";
+    public static final String STANDARD_DELIVERY_WINDOW_END = "Delivery Window End Time";
+    public static final String STANDARD_BACKDOOR_START = "Backdoor Delivery Window Start Time";
+    public static final String STANDARD_BACKDOOR_END = "Backdoor Delivery Window End Time";
+
+    public static final String PRIMARY_SHIP_TO = "700 TAUNTON";
+}
--- SUS_Test_ui/src/test/java/org/example/sus/tests/LoginTest.java
+++ SUS_Test_ui/src/test/java/org/example/sus/tests/LoginTest.java
@@ -4,6 +4,7 @@ import com.syscolabs.sus.functions.terminal5250.DataRoutingControl;
 import com.syscolabs.sus.functions.terminal5250.T5250Home;
 import org.example.sus.data.*;
 import org.example.sus.functions.sus.SUSLogin;
+import org.example.sus.functions.sus.ShipToInquiry;
 import org.example.sus.functions.sus.ShipToInquiryPrompt;
 import org.example.sus.utils.ExcelUtil;
 import org.example.sus.utils.TestBase;
@@ -33,6 +34,13 @@ public class LoginTest extends TestBase {
         T5250Home.enterQuickAccessCode(AccessCodeData.QUICK_ACCESS_CODE_SHIP_TO_MAINTENANCE,AccessCodeData.QUICK_ACCESS_TYPE,AccessCodeData.QUICK_ACCESS_NUMBER);
         ShipToInquiryPrompt.viewShipToDetails(OpCoID,"166548");
 
+        softAssert.assertEquals(ShipToInquiry.getShipToNumber(),"166548","Incorrect Ship-To number displayed in SUS");
+        boolean abc = T5250Home.verifyScreen(SUSScreenData.STI.getValue());
+        System.out.println(abc);
+
+//        softAssert.assertTrue(T5250Home.verifyScreen(SUSScreenData.STI.getValue()),"User not in "+SUSScreenData.STI.getValue()+" screen");
+//        softAssert.assertEquals(ShipToInquiry.getShipToNumber(),"166548","Incorrect Ship-To number displayed in SUS");
+//        softAssert.assertEquals(ShipToInquiry.getShipToName(),secondaryAccountShippingData.getAccountName(),"Incorrect Ship-To name displayed in SUS");
 
     }
 }
